ef8ac71f5b8a1d9ce78af107608006c95cd78377
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-02-22 14:01:24 -0500

    Swap n_heads for n_predict

--- fms_extras/modules/speculator.py
+++ fms_extras/modules/speculator.py
@@ -4,21 +4,21 @@ import torch.nn.functional as F
 from fms.modules.layernorm import LayerNormParameterized
 
 class Speculator(nn.Module):
-    def __init__(self, emb_dim=4096, inner_dim=0, vocab_size=32000, n_heads=3):
+    def __init__(self, emb_dim=4096, inner_dim=0, vocab_size=32000, n_predict=3):
         super().__init__()
-        self.nheads = n_heads
+        self.npredict = n_predict
         self.emb_dim = emb_dim
         inner_dim = inner_dim if inner_dim!=0 else emb_dim
         self.inner_dim = inner_dim
         self.vsize = vocab_size
-        self.emb = nn.ModuleList([nn.Embedding(vocab_size, inner_dim) for _ in range(n_heads)])
-        self.proj = nn.ModuleList([nn.Linear((emb_dim if i==0 else inner_dim), inner_dim, bias=False) for i in range(n_heads)])
-        self.head = nn.ModuleList([nn.Linear(inner_dim, vocab_size, bias=False) for _ in range(n_heads)])
+        self.emb = nn.ModuleList([nn.Embedding(vocab_size, inner_dim) for _ in range(n_predict)])
+        self.proj = nn.ModuleList([nn.Linear((emb_dim if i==0 else inner_dim), inner_dim, bias=False) for i in range(n_predict)])
+        self.head = nn.ModuleList([nn.Linear(inner_dim, vocab_size, bias=False) for _ in range(n_predict)])
         self.ln = nn.ModuleList(
-            [LayerNormParameterized(inner_dim, elementwise_shift=True, elementwise_scale=True) for _ in range(n_heads)]
+            [LayerNormParameterized(inner_dim, elementwise_shift=True, elementwise_scale=True) for _ in range(n_predict)]
         )
         # Weights ensure that state_0 accounts for 50% of state magnitude by final head in expectation
-        self.state_weight = .5**(.5/n_heads)
+        self.state_weight = .5**(.5/n_predict)
         self.emb_weight = (1-self.state_weight**2)**.5
         self.a = nn.GELU()
 
@@ -46,9 +46,9 @@ class Speculator(nn.Module):
         out = torch.empty(b, 1, 0, device=state.device).int()  # b k h
         log_probs = torch.zeros(b, 1, device=state.device)  # b k
         assert (
-            len(topk) == self.n_heads
-        ), f"You must provide a topk number for each head ({self.n_heads} heads, {len(topk)} provided)"
-        for i in range(self.n_heads):
+            len(topk) == self.n_predict
+        ), f"You must provide a topk number for each head ({self.n_predict} heads, {len(topk)} provided)"
+        for i in range(self.n_predict):
             # Project and predict
             z = self.emb[i](ind).mul(self.emb_weight*(self.inner_dim/2)**.5)  # b k d
             state = self.a(self.ln[i](self.proj[i](state)*self.state_weight+z))  # b k d
@@ -70,7 +70,7 @@ class Speculator(nn.Module):
         # Take only top n best guesses
         best_guesses = log_probs.topk(n, dim=1)[1]  # b k
         return out.gather(
-            1, best_guesses.unsqueeze(2).expand(-1, -1, self.n_heads)
+            1, best_guesses.unsqueeze(2).expand(-1, -1, self.n_predict)
         )  # b n h
 
     def forward(self, state, inds):
@@ -78,13 +78,13 @@ class Speculator(nn.Module):
         FOR TRAINING
         ----
         Since we're assuming all prior tokens are "correct", don't act recursively, just pull from provided inds.
-        Produces self.n_heads predicted tokens for each token embedding in state.
-        Inds requires self.n_heads extra tokens on the right to "simulate" recursive behavior for end positions.
+        Produces self.n_predict predicted tokens for each token embedding in state.
+        Inds requires self.n_predict extra tokens on the right to "simulate" recursive behavior for end positions.
         """
         # state: b n d
         # inds: b n+h (..., pred token, n+2, n+3, n+4)
         out = []
-        for i in range(self.nheads):
+        for i in range(self.npredict):
             z = self.emb[i](inds[:, i : i + state.size(1)]).mul(self.emb_weight*(self.inner_dim/2)**.5)  # b n d
             state = self.a(self.ln[i](self.proj[i](state)*self.state_weight+z))  # b n d
             out.append(self.head[i](state))  # b n v
