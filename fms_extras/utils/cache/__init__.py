0ee51338c249a802d4746a051aa1f5707a7ba9fc
Joshua Rosenkranz <joshua.rosenkranz@gmail.com>
2024-03-22 15:31:43 -0400

    now using tensors for compute_position_ids; removed the utility method for compute_position_ids as its really only used along with CacheDataWithMetaData

--- fms_extras/utils/cache/__init__.py
+++ fms_extras/utils/cache/__init__.py
@@ -5,39 +5,6 @@ from typing import List, Optional, Tuple
 import torch
 
 
-def compute_position_ids(
-    num_tokens_per_sequence: List[int], context_lengths: Optional[List[int]] = None
-) -> List[List[int]]:
-    """Compute position ids based on the current context lengths and the new tokens to add
-
-    Parameters
-    ----------
-    num_tokens_per_sequence: List[int]
-        number of tokens to be added to each sequence
-    context_lengths: List[int], optional
-        optional list of current context lengths per sequence. If none, will assume no context length and starting
-        position will be 0 (default is None)
-
-    Returns
-    -------
-    List[List[int]]
-        the position ids for each sequence
-    """
-
-    max_tokens = max(num_tokens_per_sequence)
-    position_ids = []
-    for seq_i, num_tokens in enumerate(num_tokens_per_sequence):
-        if context_lengths is None:
-            start = 0
-        else:
-            start = context_lengths[seq_i] - 1
-        position_ids_i = [0 for _ in range(max_tokens - num_tokens)] + [
-            i for i in range(start, start + num_tokens)
-        ]
-        position_ids.append(position_ids_i)
-    return position_ids
-
-
 class AttentionComputationMixin(metaclass=abc.ABCMeta):
     """
     Include this mixin in a class to implement a custom version of attention
@@ -180,18 +147,33 @@ class CacheDataWithMetadata(CacheData):
     context_lengths: Optional[torch.Tensor]
 
     def compute_position_ids(self, num_tokens_per_sequence: List[int]) -> torch.Tensor:
-        position_ids_list = compute_position_ids(
-            num_tokens_per_sequence,
-            None
-            if self.context_lengths is None
-            else [
-                r - l + 1
-                for l, r in zip(num_tokens_per_sequence, self.context_lengths.tolist())
-            ],
-        )
-        return torch.tensor(
-            position_ids_list, dtype=torch.long, device=self.data[0][0].device
-        )
+        """Compute position ids based on the current context lengths and the new tokens to add
+
+        Parameters
+        ----------
+        num_tokens_per_sequence: List[int]
+            number of tokens to be added to each sequence
+
+        Returns
+        -------
+        torch.Tensor
+            the position ids for each sequence
+        """
+        device = self.data[0][0].device
+        max_tokens = max(num_tokens_per_sequence)
+        position_ids = []
+        for seq_i, num_tokens in enumerate(num_tokens_per_sequence):
+            start = (
+                0
+                if self.context_lengths is None
+                else self.context_lengths[seq_i].item() - num_tokens
+            )
+            pads = torch.zeros(max_tokens - num_tokens, dtype=torch.long, device=device)
+            positions = torch.arange(
+                start, start + num_tokens, dtype=torch.long, device=device
+            )
+            position_ids.append(torch.cat((pads, positions)))
+        return torch.stack(position_ids)
 
 
 class KVCacheManager(metaclass=abc.ABCMeta):
